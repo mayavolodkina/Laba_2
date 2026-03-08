0c60ca25b936fa44caac3d8d777d73753a9132fa
Майя <maya.volodkina06@yandex.ru>
2026-03-08 23:36:49 +0300

    выбор 3х вариантов ввод, файл, ссылка

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World")
+def main():
+    print("Выберите режим работы:")
+    print("1 — Пользовательский ввод")
+    print("2 — Поиск в файле")
+    print("3 — Поиск на веб-странице")
+
+    choice = input("Вы выбрали: ")
+
+    if choice == '1':
+        check_user_input()
+    elif choice == '2':
+        fname = input("Введите имя файла: ")
+        check_file(fname)
+    elif choice == '3':
+        url = input("Введите URL: ")
+        check_url(url)
+    else:
+        print("Неверный выбор")
